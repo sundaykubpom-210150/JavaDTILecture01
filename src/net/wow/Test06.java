4da0a3928167e0fda30a8d22ff202a004171f9c0
sundaykubpom-210150 <sunday0581@gmail.com>
2026-01-13 16:15:59 +0700

    สร้างโปรเจ็กต์และเขียนโค้ด success

--- src/net/wow/Test06.java
+++ src/net/wow/Test06.java
@@ -1,4 +1,41 @@
 package net.wow;
 
+import java.util.Scanner;
+
 public class Test06 {
+    public static void main(String[] args) {
+        String fullname;
+        int menu;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("ป้อนชื่อ : ");
+        fullname = scanner.next();
+        System.out.println("----------------------");
+        System.out.println("    เลือกเมนูทักทาย");
+        System.out.println("----------------------");
+        System.out.println("1) Hello");
+        System.out.println("2) Hi");
+        System.out.println("3) Hey");
+        System.out.println("----------------------");
+        System.out.print("เลือกเมนู : ");
+        menu = scanner.nextInt();
+        System.out.println("----------------------");
+
+        switch ( menu ){
+            case 1 :
+                System.out.println("Hello, " + fullname);
+                break;
+            case 2 :
+                System.out.println("Hi, " + fullname);
+                break;
+            case 3 :
+                System.out.println("Hey, " + fullname);
+                break;
+            default:
+                System.out.println("เลือกเมนูผิด...!");
+        }
+
+        System.out.println("----------------------");
+    }
 }
--- src/th/ac/sau/Test05.java
+++ src/th/ac/sau/Test05.java
@@ -1,4 +1,22 @@
+// รับคะแนนเลขจำนวนเต็มทางแป้นพิมพ์ แล้วแสดงผล 50 ขึ้นไปผ่าน ไม่ถึงไม่ผ่าน
 package th.ac.sau;
 
+import java.util.Scanner;
+
 public class Test05 {
+    public static void main(String[] args) {
+        int score;
+        String result;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("ป้อนคะแนน : ");
+        score = scanner.nextInt();
+        System.out.println("----------------------------");
+
+        result = score >= 50 ? "ผ่าน" : "ไม่ผ่าน";
+
+        System.out.println("ผลคะแนนคือ : " + result);
+
+    }
 }
